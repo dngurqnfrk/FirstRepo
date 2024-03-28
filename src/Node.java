ff9905b76eacc2bc20cfe03ae1407b7e9ab15207
dngurqnfrk <jwh1090@gmail.com>
2024-03-29 00:26:48 +0900

    Normal structure for utilizing FP-Growth Alg.
    1. Node Class is for FP-Tree
    Maybe set Node as variable in class FP_Growth
    
    2. FP_Growth Class is for All about algorithm
    From reading csv file to interpreting FP_Tree

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,38 @@
 public class Node {
-    Node LeftChild;
-    Node RightSibling;
+    private Node leftChild;
+    private Node rightSibling;
+    private String product;
+    private Integer count;
+    private Node next;
 
+
+    // Constructor
+    Node() {
+        leftChild = null;
+        rightSibling = null;
+        product = null;
+        count = 0;
+        next = null;
+    }
+
+    //
+    void SetLeftChild(Node left) {
+        leftChild = left;
+    }
+
+    void SetRightSibling(Node right) {
+        rightSibling = right;
+    }
+
+    void SetProduct(String s) {
+        product = s;
+    }
+
+    void SetCount(Integer d) {
+        count = d;
+    }
+
+    void SetNext(Node n) {
+        next = n;
+    }
 }
