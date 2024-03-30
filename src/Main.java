782761a3919c5b1590fefafc09bf06e621ac987b
dngurqnfrk <jwh1090@gmail.com>
2024-03-30 22:40:19 +0900

    v 2.01 I make FP-Growth Code. If we submit this code, We erase the code with comment "// Erase" and "// debugging"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        v 2.01
+        I make FP-Growth Code.
+        If we submit this code, We erase the code with comment "// Erase" and "// debugging"
+        And maybe this code don't have enough ability because I didn't use Header Table.
+        It's so hard
+         */
+
         //argument process
         String filePath = args[0];
         float MSV = Float.parseFloat(args[1]);
